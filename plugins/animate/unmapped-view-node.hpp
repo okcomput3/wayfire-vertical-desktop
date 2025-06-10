5e86d2657d14e5cb647f8b92a7dafbe293090cf2
Ilia Bozhinov <ammen99@gmail.com>
2025-06-10 23:35:03 +0200

    animate: properly handle unmapped node position
    It is possible that the main view changes its position.

--- plugins/animate/unmapped-view-node.hpp
+++ plugins/animate/unmapped-view-node.hpp
@@ -10,18 +10,24 @@ namespace wf
 class unmapped_view_snapshot_node : public wf::scene::node_t
 {
     wf::auxilliary_buffer_t snapshot;
-    wf::geometry_t bbox;
+    std::weak_ptr<wf::view_interface_t> _view;
 
   public:
     unmapped_view_snapshot_node(wayfire_view view) : node_t(false)
     {
         view->take_snapshot(snapshot);
-        bbox = view->get_surface_root_node()->get_bounding_box();
+        _view = view->weak_from_this();
     }
 
     wf::geometry_t get_bounding_box() override
     {
-        return bbox;
+        if (auto view = _view.lock())
+        {
+            auto current_bbox = view->get_surface_root_node()->get_bounding_box();
+            return wf::construct_box(wf::origin(current_bbox), snapshot.get_size());
+        }
+
+        return {0, 0, 0, 0};
     }
 
     void gen_render_instances(std::vector<scene::render_instance_uptr>& instances,
