7091f03c95ddc182b9a59d670dbef6923e4bacf4
Scott Moreau <oreaus@gmail.com>
2025-06-14 11:03:51 -0600

    ipc-rules: Only allow subscriptions for valid signal names (#2656)

--- plugins/ipc-rules/ipc-events.hpp
+++ plugins/ipc-rules/ipc-events.hpp
@@ -153,6 +153,9 @@ class ipc_rules_events_methods_t : public wf::per_output_tracker_mixin_t<>
                 if (signal_map.count(sub))
                 {
                     subscribed_to.insert(sub);
+                } else
+                {
+                    return wf::ipc::json_error("Event not found: \"" + sub.as_string() + "\"");
                 }
             }
         } else
